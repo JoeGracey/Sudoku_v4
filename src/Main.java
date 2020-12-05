67e7cfa332e4b28a07c8097cc3c3d880abff8139
Joe Gracey <joe.gracey@gmail.com>
2020-12-04 22:57:51 -0700

    solvedAndUnsolved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
+        int[][] sudokuBoard = {
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+        printUnsolvedBoard(sudokuBoard, sudokuBoard.length);
+    }
+
+    public static void printUnsolvedBoard() {
+
+    }
+
+    public static void printSolvedBoard() {
+
     }
 }
