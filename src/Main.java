2179775e06586b922c7a20c5eb5d183db312939d
Joe Gracey <joe.gracey@gmail.com>
2020-12-09 00:07:11 -0700

    solveAndUnsolved

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] sudokuBoard = {
-                {3, 0, 6, 5, 0, 8, 4, 0, 0},
-                {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+                {8, 0, 0, 9, 3, 0, 0, 0, 2},
+                {0, 0, 9, 0, 0, 0, 0, 4, 0},
+                {7, 0, 2, 1, 0, 0, 9, 6, 0},
+                {2, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 6, 0, 0, 0, 0, 0, 7, 0},
+                {0, 7, 0, 0, 0, 6, 0, 0, 5},
+                {0, 2, 7, 0, 0, 8, 4, 0, 6},
+                {0, 3, 0, 0, 0, 0, 5, 0, 0},
+                {5, 0, 0, 0, 6, 2, 0, 0, 8}
         };
 
         printUnsolvedBoard(sudokuBoard, sudokuBoard.length);
         printSolvedBoard(sudokuBoard, sudokuBoard.length);
     }
 
-    public static void printUnsolvedBoard(int[][] SudokuBoard, int moreBlah) {
-        for (int i = 0; i < moreBlah; i++) {
+    public static void printUnsolvedBoard(int[][] SudokuBoard, int sudokuBoardLength) {
+        for (int i = 0; i < sudokuBoardLength; i++) {
             if ((i % 3 == 0) && (i != 0)) {
                 System.out.println("----------|unsolved|----------");
             }
-            for (int j = 0; j < moreBlah; j++) {
+            for (int j = 0; j < sudokuBoardLength; j++) {
                 if (j % 3 == 0) {
                     System.out.print("|");
                 }
@@ -38,12 +38,12 @@ public class Main {
         System.out.println();
     }
 
-    public static void printSolvedBoard(int[][] SudokuBoard, int moreBlah) {
-        for (int i = 0; i < moreBlah; i++) {
+    public static void printSolvedBoard(int[][] SudokuBoard, int sudokuBoardLength) {
+        for (int i = 0; i < sudokuBoardLength; i++) {
             if ((i % 3 == 0) && (i != 0)) {
                 System.out.println("----------|solved|----------");
             }
-            for (int j = 0; j < moreBlah; j++) {
+            for (int j = 0; j < sudokuBoardLength; j++) {
                 if (j % 3 == 0) {
                     System.out.print("|");
                 }
